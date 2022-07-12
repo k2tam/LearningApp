1acd1804dc12641982a9c76844e35a267b066fab
k2tam <buingockhaitam01@gmail.com>
2022-07-12 22:31:09 +0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by k2 tam on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
