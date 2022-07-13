a8c3de583d2b3622e323965bb4d0421a4b9032f2
k2tam <buingockhaitam01@gmail.com>
2022-07-13 17:07:25 +0700

    - adding assets to resources - add models - parse data json

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -8,5 +8,45 @@
 import Foundation
 
 class ContentModel : ObservableObject {
+    @Published var modules = [Module]()
     
+    init(){
+        getLocalData()
+    }
+    
+    func getLocalData() -> [Module]{
+        //MARK: Get local json data
+        
+        //get url to the json file
+        let url = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        
+        //Return empty module array if url is nil
+        guard url != nil else{
+            return [Module]()
+        }
+        
+        
+        do{
+            //Get data object from the contents of url
+            let data = try Data(contentsOf: url!)
+            
+            //Try to decode data object to [Module]
+            let decoder = JSONDecoder()
+            
+            
+            do{
+                let modules = try decoder.decode([Module].self, from: data)
+                self.modules = modules
+
+            }catch{
+                print("decode data error")
+            }
+            
+        }catch{
+            print("data object error")
+        }
+        
+        return [Module]()
+    }
 }
